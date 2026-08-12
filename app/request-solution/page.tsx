a581809de9195d00d5c962b8faa5d0a20562fa42
kiber-cyber <webgambit@gmail.com>
2026-08-12 05:40:53 +0400

    aham

--- app/request-solution/page.tsx
+++ app/request-solution/page.tsx
@@ -17,7 +17,7 @@ export async function generateMetadata(): Promise<Metadata> {
       title: dict.meta.title,
       description: dict.meta.description,
       url: "https://mwaysolutions.net/request-solution",
-      siteName: "MWAY Solutions",
+      siteName: "MWAYSolutions",
     },
   };
 }
